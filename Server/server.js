a2dc789271744296f6ac15e337e073e05c87421b
Joel T George <joelthomas0203@gmail.com>
2023-02-12 19:40:07 +0530

    socket installed and little try

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const WebSocket = require("ws");
+
+
+
 const server = express();
-
 const router = express.Router();
+const PORT = 9000;
 
-router.get("/", function(req,res){
-    res.send("index.html")
+wss = new WebSocket.Server({port: 9000, host:'192.168.137.1'})
+
+router.get("/Dashboard", function(req, res){
+    res.send("dashborad");
+
 })
 
+
+router.get("/", function(req,res){
+    var clientIP = req.ip;
+    console.log("requested client Ip: "+ clientIP)
+    res.send("joel is coding")
+})
+
+wss.on("connection", function (ws){
+    ws.on("message", function (data){
+      
+        console.log('recived: %s', data)
+    })
+    ws.send("funny")
+    
+})
 server.use("/",router)
-server.listen(9000, function(){
+server.listen(PORT, function(){
     console.log("running on 9000")
+  
 })
